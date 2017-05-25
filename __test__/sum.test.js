6b015b44c58053d3402fd3292a746cfd796f7cd3
Enrique Arias <emoriarty81@gmail.com>
2017-05-25 12:36:42 +0200

    Matching sum result against stored snapshot

--- __test__/sum.test.js
+++ __test__/sum.test.js
@@ -1,3 +1,8 @@
 import sum from '../src/sum';
 
-test('1 + 2 is equal to 3', () => expect(sum(1, 2)).toBe(3))
+describe('testing sum function', () => {
+  // Checks the result of sum against a constant value
+  test('1 + 2 is equal to 3', () => expect(sum(1, 2)).toBe(3));
+  // Checks the result of sum against a stored output snapshout
+  test('2 + 2 is equal to 4', () => expect(sum(2, 2)).toMatchSnapshot());
+});
