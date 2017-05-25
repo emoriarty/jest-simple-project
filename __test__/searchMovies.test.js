181d3b8880a194d16c2eee068e346b79a068d86d
Enrique Arias <emoriarty81@gmail.com>
2017-05-25 14:27:10 +0200

    Updated tests

--- __test__/searchMovies.test.js
+++ __test__/searchMovies.test.js
@@ -1,12 +1,14 @@
 import searchMovies from '../src/searchMovies';
 
 describe('testing searchMovies function', () => {
-  // Test against stored output
+
   test('by title', () =>
     expect(searchMovies.byTitle('prometheus')).toMatchSnapshot())
 
-  test('is case insensitive', () =>
-    expect(searchMovies.byTitle('prometheus').length).toBe(1))
+  test('is case insensitive', () => {
+    expect(searchMovies.byTitle('prometheus').length).toBe(1);
+    expect(searchMovies.byTitle('Prometheus').length).toBe(1);
+  })
 
   test('sequencial search', () =>
     ['a', 'alien', 'alien 3'].forEach((query) =>
